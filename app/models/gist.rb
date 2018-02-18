58ba53b68e4fdb6abdf023bb25a2f667bb2ecb97
Wlodek Bzyl <matwb@univ.gda.pl>
2018-02-18 19:29:55 +0100

    Add more validations to Gist model

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -1,4 +1,6 @@
 class Gist < ApplicationRecord
   has_many :comments
+
   validates :src, presence: true, length: { minimum: 8, maximum: 256 }
+  validates :lang, presence: true, length: { maximum: 16 }
 end
