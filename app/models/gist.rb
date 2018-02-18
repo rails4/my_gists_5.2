f2280e66b35604905af1cc601b77140599aeb002
Wlodek Bzyl <matwb@univ.gda.pl>
2018-02-18 18:24:06 +0100

    Update

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -1,2 +1,4 @@
 class Gist < ApplicationRecord
+  has_many :comments
+  validates :src, presence: true, length: { minimum: 8, maximum: 256 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'gists#index'
-  resources :gists
+
+  resources :gists do
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
